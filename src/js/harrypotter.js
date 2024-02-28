24fa931331b81aba8a1b3d64799063e4e7254c5e
Ronja Norlén <rono2300@student.miun.se>
2024-02-28 17:12:50 +0100

    Klickfunktion för att visa mer info om husen

--- src/js/harrypotter.js
+++ src/js/harrypotter.js
@@ -10,23 +10,26 @@ const housesURL = 'https://wizard-world-api.herokuapp.com/Houses';
 
 window.onload = init();
 
-// async function init() {
-  //  try {
-    //    const response = await fetch(characterUrl);
-      //  let characterData = await response.json();
+//
 
-        //console.log(characterData);
-       // displayCharacters(characterData);
+ // async function characters() {
+   // try {
+   // const response = await fetch(characterUrl);
+    //   let characterData = await response.json();
+// TA BORT INNAN PUBLICERING
+      //  console.table(characterData);
+
+        // Ta med data till ny funktion
+        // displayCharacters(characterData);
 
    // } catch (e) {
-     //   console.log(e);
-      //  document.getElementById('error').innerHTML = "<p>Kunde inte hitta karaktärer</p>";
+    //    console.log(e);
+     //   document.getElementById('error').innerHTML = "<p>Kunde inte hitta karaktärer</p>";
    // }
 // }
-// function displayCharacters(characterData) {
 
-// }
 
+// Hämta de 4 husen
 async function init() {
     try {
         const response = await fetch (housesURL);
@@ -41,15 +44,37 @@ async function init() {
     } catch (e) {
         console.log(e)
         document.getElementById('error').innerHTML = "<p>Problemos</p>";
+        
     }
 }
 
+// Visa de 4 husen
 function displayHouses(houseData) {
-   const houseDataEL = document.getElementById('houses');
+    const houseDataEL = document.getElementById('houses');
 
-   // Loopa igenom och skriv ut i lista
-   houseData.forEach((house) => {
-    houseDataEL.innerHTML += `<li>${ house.name }</li>`;
-   });
-   
+    houseData.forEach((house) => {
+        // Skapa ett nytt li-element för varje hus
+        const listItem = document.createElement('li');
+        listItem.textContent = house.name;
+
+        // Lägg till en klickhändelsehanterare på varje li-element
+        listItem.addEventListener('click', () => {
+            // Anropa funktionen för att visa mer info när ett hus klickas på
+            showHouseInfo(house);
+        });
+
+        // Lägg till det nya li-elementet till listan av hus
+        houseDataEL.appendChild(listItem);
+    });
 }
+
+function showHouseInfo(house) {
+    const houseInfo = document.getElementById('house-info');
+    houseInfo.innerHTML = `
+        <h2>${house.name}</h2>
+        <p>Founder: ${house.founder}</p>
+        <p>House Ghost: ${house.ghost}</p>
+        
+    `;
+}
+
