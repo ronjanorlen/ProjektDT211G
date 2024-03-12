0d7c7b7fb27d652049b221af5fe40cd71519e44a
Ronja Norlén <rono2300@student.miun.se>
2024-03-12 10:53:01 +0100

    Skapat ny sida för trollformler

--- src/js/spells.js
+++ src/js/spells.js
@@ -1,15 +1,18 @@
 "use strict";
 
 const spellURL = 'https://wizard-world-api.herokuapp.com/Spells';
+let spellData; 
 
 window.onload = init();
 
-// Hämta de 4 husen
+
+// Hämta trollformler
 async function init() {
     try {
         const response = await fetch (spellURL);
-        let spellData = await response.json();
+        spellData = await response.json(); 
 
+        // TA BORT INNAN PUBLICERING
         console.table(spellData);
 
         // Ta med data till ny funktion
@@ -22,7 +25,40 @@ async function init() {
     }
 }
 
-// Visa trollformler
+// Visa trollformler med trollformelnamn + vad den gör
 function displaySpells(spellData) {
-    const spellDataEL = document.getElementById("spells");
-} 
+    const spellDataEL = document.getElementById('spells');
+
+    // Skapa en div för att hålla trollformlerna
+    const spellContainer = document.createElement('div');
+    spellContainer.id = 'spellContainer';
+    
+    // Loopa igenom varje trollformel i spellData
+    spellData.forEach((spell) => {
+        // Kontrollera att incantation och effect är definierade
+        if (spell.incantation && spell.effect) {
+            // Skapa en h4 för incantation
+            const incantationHeader = document.createElement('h4');
+            incantationHeader.textContent = `Incantation: ${spell.incantation}`;
+            
+            // Skapa <p> för effect
+            const effectParagraph = document.createElement('p');
+            effectParagraph.textContent = `Effect: ${spell.effect}`;
+            
+            // Skapa en container för varje trollformel
+            const spellItem = document.createElement('div');
+            spellItem.classList.add('spellItem');
+
+            // Lägg till incantation och effect till spellItem
+            spellItem.appendChild(incantationHeader);
+            spellItem.appendChild(effectParagraph);
+            
+            // Lägg till spellItem till spellContainer
+            spellContainer.appendChild(spellItem);
+        }
+    });
+
+    // Lägg till spellContainer till spellDataEL
+    spellDataEL.appendChild(spellContainer);
+}
+
